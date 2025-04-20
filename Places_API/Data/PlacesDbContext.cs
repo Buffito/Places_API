dfac88a5807540fb6455fc21e0fd8312db77cabc
Buffito <thodoriskotoufos@hotmail.com>
2025-04-20 22:56:56 +0300

    Models created Basic structure created

---
+++ Places_API/Data/PlacesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Places_API.Models;
+
+namespace Places_API.Data
+{
+    public class PlacesDbContext : DbContext
+    {
+        public PlacesDbContext(DbContextOptions<PlacesDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Place>().ToTable("Places");
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+}
--- Places_API/Program.cs
+++ Places_API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,21 +10,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(); // Ensure Swashbuckle.AspNetCore package is installed
+builder.Services.AddDbContext<PlacesDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); // Ensure Microsoft.EntityFrameworkCore.SqlServer package is installed
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Secret"]))
+        };
+    });
+builder.Services.AddAuthorization();
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
